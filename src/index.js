47dacbf5a670945d5649dc37a8c1e61de6b5fa9a
bami55 <1992bami@gmail.com>
2019-12-27 19:05:56 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
+import Tiger from './utilities';
+
+console.log(Tiger.say());
 
 function component () {
     const element = document.createElement('div');
-    const array = ['Hello', 'webpack', '!!'];
+    const array = ['Hello', 'webpack', '!'];
     element.innerHTML = _.join(array, ' ');
     return element;
 }
